f961e3e0b4ffb21f2d3bbf8eff2417f3cad2486d
gtarlin <gtarlin@gmail.com>
2020-06-09 02:58:24 -0400

    added js scrollling in response to review

--- README.md
+++ README.md
@@ -2,10 +2,11 @@
 
 ## Notes
 
+I replaced the scrolling mechanism.
+I had previously used a css property to achieve the effect, but I switched it to javascript to satisfy the rubric. 
+
 I used the provided HTML and CSS styling to start the Landing Page project.
 
 I added javascript to select the section elements and use the information in those to create navigation in the header.
 
 I used javascript to add an eventlistener to each section to check to see if it was in the window and made it active if it was. I also added styling to the cooresponding menu item as well to indicate that it was active. I didn't add any extra styling to the active sections beyond what was already in place in the existing css (gradient background and turning on the animated circles). If you want me to, I can add more styling in that section of the css.
-
-I created the scrolling action (rather than jumping when clicking the nav links)  by using the "scroll-behavior: smooth" attribute in the html tag in the css file.  Should I have done this in a more difficult way using javascript to manually reproduce that simple behavior?
--- js/app.js
+++ js/app.js
@@ -34,46 +34,41 @@ const allSections = document.querySelectorAll('section');
 allSections.forEach(createNav);
 
 function createNav(section) {
-  //create new list item
   var newLi = document.createElement("li");
-  //create new anchor tag
   var newA = document.createElement("a");
-  //use data-nav attribute of section to get label for link
   newA.innerHTML = (section.getAttribute("data-nav"));
-  //create link to section
   newA.href = `#${section.id}`;
-  //add class of menu__link to turn on built in styling from css
   newA.classList.add("menu__link");
-  //create id for the li based on section id
   newLi.id = (`li_${section.id}`);
   var ulElement = document.getElementById("navbar__list");
-  //add anchor element to list element
   newLi.appendChild(newA);
-  //add list element to UL
   ulElement.appendChild(newLi);
+
+  //add scrolling feature when nav is clicked
+  newA.addEventListener('click', (event) => {
+    event.preventDefault();
+    section.scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+      inline: "nearest"
+    });
+  });
 }
 
 
-// Add class 'active' to section when near top of viewport
-
+// Add class 'isActive' to section when near top of viewport
 //loop through all section elements, running addListener function for each one
 allSections.forEach(addListener);
 
 function addListener(section) {
-  //create event listener on window object
   window.addEventListener('scroll', () => {
-    //find bounding box of section
     var bbox = section.getBoundingClientRect();
-    //check to see if the bounding box is within limits and is not yet active
-    if (bbox.top <= 130 && bbox.bottom >= 130 && !section.classList.contains("isActive")) {
-      //add class isActive to section to turn on styling
+    if (bbox.top <= 175 && bbox.bottom >= 175 && !section.classList.contains("isActive")) {
       section.classList.add("isActive");
-      //add activeLi class to anchor within li to turn on styling
+      //also add class to anchor within li in nav
       const liActive = document.getElementById(`li_${section.id}`);
       liActive.firstChild.classList.add("activeLi");
-    //check to see if section if out of bounds but still has class isActive
-    } else if ((bbox.top > 130 || bbox.bottom < 130) && section.classList.contains("isActive")) {
-      //if so remove classes from section and from anchor within li
+    } else if ((bbox.top > 175 || bbox.bottom < 175) && section.classList.contains("isActive")) {
       section.classList.remove("isActive");
       const liActive = document.getElementById(`li_${section.id}`);
       liActive.firstChild.classList.remove("activeLi");
